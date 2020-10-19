18e3d31daa1e8becdfb0759db4d635e0f30e4ea3
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-20 00:15:36 +0530

    recommending the best match

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))  
 
@@ -8,8 +9,16 @@ def translate(word):
     
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff = 0.8)) > 0:
+        yn = input(f"Did you mean {get_close_matches(word, data.keys(), n = 2, cutoff = 0.8)[0]} instead? Enter Y if yes, or N if no: ")
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys(), n = 2, cutoff = 0.8)[0]]
+        elif yn == "N":
+            return "Word does not exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
-        return "Word does not exist.Please double check it"
+        return "Word does not exist. Please double check it"
 
 word = input("Enter word: ")
 
