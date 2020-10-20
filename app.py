54d34ba50acb58e29b2a8ce750ea7f0ed0817d2e
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-20 13:58:02 +0530

    final code

--- app.py
+++ app.py
@@ -9,17 +9,27 @@ def translate(word):
     
     if word in data:
         return data[word]
+
     elif len(get_close_matches(word, data.keys(), cutoff = 0.8)) > 0:
+        
         yn = input(f"Did you mean {get_close_matches(word, data.keys(), n = 2, cutoff = 0.8)[0]} instead? Enter Y if yes, or N if no: ")
+        
         if yn == "Y":
             return data[get_close_matches(word, data.keys(), n = 2, cutoff = 0.8)[0]]
         elif yn == "N":
             return "Word does not exist. Please double check it."
         else:
             return "We didn't understand your entry."
+    
     else:
         return "Word does not exist. Please double check it"
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
